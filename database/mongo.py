21c515fa413343296137be7d62f21721ea980e7f
temankuya <125769735+temankuya@users.noreply.github.com>
2025-07-21 17:37:24 +0700

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,30 +1,24 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-
 from config import MONGO_URL
+
+if not MONGO_URL:
+    raise ValueError("MONGO_URL belum disetel di config.env")
 
 mongo_client = AsyncIOMotorClient(MONGO_URL)
 mongodb = mongo_client.fsub
-
 usersdb = mongodb.user
 
 
-
 async def is_served_user(user_id: int) -> bool:
-    user = await usersdb.find_one({"user_id": user_id})
-    if not user:
-        return False
-    return True
+    return await usersdb.find_one({"user_id": user_id}) is not None
 
 
-async def get_served_users() -> list:
-    users_list = []
-    async for user in usersdb.find({"user_id": {"$gt": 0}}):
-        users_list.append(user)
-    return users_list
+async def get_served_users() -> list[int]:
+    return [user["user_id"] async for user in usersdb.find({"user_id": {"$gt": 0}})]
 
 
 async def add_served_user(user_id: int):
-    is_served = await is_served_user(user_id)
-    if is_served:
+    if await is_served_user(user_id):
         return
-    return await usersdb.insert_one({"user_id": user_id})
+    result = await usersdb.insert_one({"user_id": user_id})
+    return result.inserted_id
