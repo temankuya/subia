3bd816211b8c0467058b73a0e6723c7dd507dd27
temankuya <125769735+temankuya@users.noreply.github.com>
2025-07-21 22:37:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import asyncio
+from pyrogram import idle
 from bot import Bot
 
+async def main():
+    bot = Bot()
+    await bot.start()
+    await idle()
+    await bot.stop()
+
 if __name__ == "__main__":
-    Bot().run()
+    asyncio.run(main())
